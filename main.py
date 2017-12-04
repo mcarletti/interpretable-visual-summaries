74156ab24f29d96f3ef855879c9b9369a48f3bb5
mcarletti <marco.carletti@univr.it>
2017-12-04 14:09:01 +0100

    Add blur parameter; Save loss history

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import argparse
 import torch
 import numpy as np
 import cv2
@@ -13,6 +12,8 @@ from regularizers import l1_reg, tv_reg, less_reg, lasso_reg
 from recordclass import recordclass # mutable version of namedtuple
 
 
+HAS_CUDA = torch.cuda.is_available()
+
 Params = recordclass('Params', [
                             'learning_rate',
                             'max_iterations',
@@ -23,7 +24,20 @@ Params = recordclass('Params', [
                             'lasso_coeff',
                             'noise_sigma',
                             'noise_scale',
+                            'blur',
                             'target_shape'])
+
+
+def get_params():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--modelname',    type=str, default='alexnet')
+    parser.add_argument('--input_image',  type=str, default='examples/original/flute3.jpg')
+    parser.add_argument('--dest_folder',  type=str, default='results/tmp')
+    parser.add_argument('--results_file', type=str, default='results/tmp/results.csv')
+    parser.add_argument('--super_pixel',  action='store_false')
+    args = parser.parse_args()
+    return args
 
 
 def load_mask(filename, target_shape=None, mode='uniform'):
@@ -35,50 +49,49 @@ def load_mask(filename, target_shape=None, mode='uniform'):
     else:
         assert target_shape is not None
         assert mode in ['uniform', 'circular']
-        if mode == 'uniform':
-            mask_init = np.ones(target_shape, dtype = np.float32)
+        # 0 = perturbate, 1 = do not perturbate
+        mask_init = np.zeros(target_shape, dtype = np.float32)
         if mode == 'circular':
-            radius = target_shape[0] # assuming square shape
+            radius = target_shape[0] // 2 # assuming square shape
             cx, cy = target_shape[0] // 2, target_shape[1] // 2
             x, y = np.ogrid[-cx:target_shape[0]-cx, -cy:target_shape[1]-cy]
-            perturbed_pixels = x*x + y*y <= radius*radius
-            mask_init = np.zeros(target_shape, dtype = np.float32)
+            perturbed_pixels = x*x + y*y > radius*radius
             mask_init[perturbed_pixels] = 1.
     return mask_init
 
 
+def compute_stats(target, output, blurred):
+    drop = (target - output) / target
+    p = (output - target) / (target - blurred)
+    return drop, p
+
+
 if __name__ == '__main__':
+
+    #--------------------------------------------------------------------------
 
     t_start = time.time()
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--input_image', type=str, default='examples/original/unicycle2.jpg')
-    parser.add_argument('--dest_folder', type=str, default='results/tmp')
-    parser.add_argument('--results_file', type=str, default='results/tmp/results.csv')
-    parser.add_argument('--super_pixel', action='store_true')
-
-    args = parser.parse_args()
+    args = get_params()
 
     data = ''
     if not os.path.exists(args.results_file):
         data = 'filename, target_prob, smooth_mask_prob, smooth_drop, smooth_blurred_prob, smooth_p, sharp_mask_prob, sharp_drop, sharp_blurred_prob, sharp_p, spx_mask_prob, spx_drop, spx_blurred_prob, spx_p\n'
 
-    use_cuda = torch.cuda.is_available()
-
     print('Loading model')
-    model = utils.load_model(use_cuda)
-    target_shape = (224, 224)
+    model, target_shape = utils.load_model(args.modelname, HAS_CUDA)
 
     # load the BGR image, resize it to the right resolution
     original_img = cv2.imread(args.input_image, 1)
     original_img = cv2.resize(original_img, target_shape)
 
-    print('*' * 12)
-    print('Computing blurred heatmap')
+    #--------------------------------------------------------------------------
+
+    print('*' * 12 + '\nComputing blurred heatmap')
 
     params = Params(
         learning_rate = 0.1,
-        max_iterations = 300,
+        max_iterations = 100,
         tv_beta = 3,      # exponential tv factor
         l1_coeff = 0.01,  # 1e-4,  # reduces number of masked pixels
         tv_coeff = 0.2,   # 1e-2,  # encourages compact and smooth heatmaps
@@ -86,6 +99,7 @@ if __name__ == '__main__':
         lasso_coeff = 0., # force masked pixels to binary values
         noise_sigma = 0., # sigma of additional perturbation
         noise_scale = 1., # scale factor of additional perturbation
+        blur = True,      # blur upsampled mask during optimization
         target_shape = target_shape)
 
     # assume to mask all the pixels (all ones)
@@ -97,61 +111,64 @@ if __name__ == '__main__':
     mask_shape = (target_shape[0] // resize_factor, target_shape[1] // resize_factor)
     mask_init = load_mask(None, mask_shape, 'circular')
 
-    results = explain.compute_heatmap(model, original_img, params, mask_init, use_cuda)
-    upsampled_mask, blurred_img_numpy, target_prob, output_prob, blurred_prob = results
+    results = explain.compute_heatmap(model, original_img, params, mask_init, HAS_CUDA)
+    upsampled_mask, blurred_img_numpy, target_prob, output_prob, blurred_prob, history = results
 
     print('Prediction drops to {:.6f}'.format(output_prob))
     out = os.path.join(args.dest_folder, 'smooth')
     utils.save(original_img, blurred_img_numpy, upsampled_mask, dest_folder=out)
 
-    smooth_drop = (target_prob - output_prob) / target_prob
-    smooth_p = (output_prob - target_prob) / (target_prob - blurred_prob)
+    smooth_drop, smooth_p = compute_stats(target_prob, output_prob, blurred_prob)
     data += args.input_image + ',' + str(target_prob) + ',' + str(output_prob) + ',' + str(smooth_drop) + ',' + str(blurred_prob) + ',' + str(smooth_p)
 
-    print('*' * 12)
-    print('Computing sharp heatmap (1)')
+    # save loss history
+    tmp = 'total_loss, class_loss, l1_loss, tv_loss, lasso_loss, less_loss\n'
+    for t in history:
+        t = [str(v) + ',' for v in t]
+        t = ''.join(t) + '\n'
+        tmp += t
+    with open(os.path.join(args.dest_folder, 'smooth/loss_history.csv'), 'w') as fp:
+        fp.write(tmp)
 
-    # we just want to start from a smart mask, which is a raw version
-    # of the previous optimization
-    # the main difference beside the lower number of iterations is the
-    # higher total variation coefficient: the higher the tv, the lower
-    # the number of perturbed regions
-    params.max_iterations = 75
-    params.tv_coeff = 1.
-    params.noise_sigma = 0.2
+    #--------------------------------------------------------------------------
+
+    print('*' * 12 + '\nComputing sharp heatmap')
+
+    params.max_iterations = 50
+    params.learning_rate = 0.5
+    params.tv_beta = 7
+    params.l1_coeff = 0.1
+    params.lasso_coeff = 1.
+    params.blur = False
 
     # we want to generate a sharp heatmap to simplify the region
     # explanation and object detection/segmentation
     # since working on a high resolution mask causes scattered
     # results, we use as initialization mask the low resolution
     # output from the previous optimization (reference paper)
-    mask_init = load_mask(None, mask_shape, 'circular')
-    #mask_path = os.path.join(args.dest_folder, 'smooth/mask.png')
-    #mask_init = load_mask(mask_path, target_shape, 'circular')
+    mask_path = os.path.join(args.dest_folder, 'smooth/mask.png')
+    mask_init = load_mask(mask_path)
 
-    results = explain.compute_heatmap(model, original_img, params, mask_init, use_cuda, verbose=False)
-    upsampled_mask = results[0]
-
-    print('Computing sharp heatmap (2)')
-
-    params.max_iterations = 225
-    params.l1_coeff = 0.1
-    params.tv_coeff = 0.2
-    params.lasso_coeff = 1.
-    params.noise_sigma = 0.
-
-    mask_init = upsampled_mask.data.cpu().squeeze().numpy()
-
-    results = explain.compute_heatmap(model, original_img, params, mask_init, use_cuda, verbose=False)
-    upsampled_mask, blurred_img_numpy, target_prob, output_prob, blurred_prob = results
+    results = explain.compute_heatmap(model, original_img, params, mask_init, HAS_CUDA, verbose=False)
+    upsampled_mask, blurred_img_numpy, target_prob, output_prob, blurred_prob, history = results
 
     print('Prediction drops to {:.6f}'.format(output_prob))
     out = os.path.join(args.dest_folder, 'sharp')
     utils.save(original_img, blurred_img_numpy, upsampled_mask, dest_folder=out)
 
-    sharp_drop = (target_prob - output_prob) / target_prob
-    sharp_p = (output_prob - target_prob) / (target_prob - blurred_prob)
+    sharp_drop, sharp_p = compute_stats(target_prob, output_prob, blurred_prob)
     data += ',' + str(output_prob) + ',' + str(sharp_drop) + ',' + str(blurred_prob) + ',' + str(sharp_p)
+
+    # save loss history
+    tmp = 'total_loss, class_loss, l1_loss, tv_loss, lasso_loss, less_loss\n'
+    for t in history:
+        t = [str(v) + ',' for v in t]
+        t = ''.join(t) + '\n'
+        tmp += t
+    with open(os.path.join(args.dest_folder, 'sharp/loss_history.csv'), 'w') as fp:
+        fp.write(tmp)
+
+    #--------------------------------------------------------------------------
 
     '''
     def compute_random_perturbation():
@@ -189,33 +206,42 @@ if __name__ == '__main__':
     print('Drop over', nb_random_tests, 'random perturbation [mean, var]:', (np.mean(rand_drops), np.var(rand_drops)))
     '''
 
+    #--------------------------------------------------------------------------
+
     if args.super_pixel:
-        print('*' * 12)
-        print('Computing superpixel heatmap')
+        print('*' * 12 + '\nComputing superpixel heatmap')
 
         params.tv_beta = 3
-        params.l1_coeff = 0.05
-        params.tv_coeff = 0.5
+        params.l1_coeff = 0.15
         params.lasso_coeff = 2.
 
-        mask_path = os.path.join(args.dest_folder, 'sharp/mask.png')
+        mask_path = os.path.join(args.dest_folder, 'smooth/mask.png')
         mask_init = load_mask(mask_path)
 
-        results = explain.compute_heatmap_using_superpixels(model, original_img, params, mask_init, use_cuda, verbose=False)
-        upsampled_mask, blurred_img_numpy, target_prob, output_prob, blurred_prob = results
+        results = explain.compute_heatmap_using_superpixels(model, original_img, params, mask_init, HAS_CUDA, verbose=False)
+        upsampled_mask, blurred_img_numpy, target_prob, output_prob, blurred_prob, history = results
 
         print('Prediction drops to {:.6f}'.format(output_prob))
         out = os.path.join(args.dest_folder, 'superpixel')
         utils.save(original_img, blurred_img_numpy, upsampled_mask, dest_folder=out)
 
-        spx_drop = (target_prob - output_prob) / target_prob
-        spx_p = (output_prob - target_prob) / (target_prob - blurred_prob)
+        spx_drop, spx_p = compute_stats(target_prob, output_prob, blurred_prob)
         data += ',' + str(output_prob) + ',' + str(spx_drop) + ',' + str(blurred_prob) + ',' + str(spx_p)
+
+        # save loss history
+        tmp = 'total_loss, class_loss, l1_loss, tv_loss, lasso_loss, less_loss\n'
+        for t in history:
+            t = [str(v) + ',' for v in t]
+            t = ''.join(t) + '\n'
+            tmp += t
+        with open(os.path.join(args.dest_folder, 'superpixel/loss_history.csv'), 'w') as fp:
+            fp.write(tmp)
     else:
         data += ',Inf,Inf,Inf,Inf'
 
-    print('*' * 12)
-    print('Saving results')
+    #--------------------------------------------------------------------------
+
+    print('*' * 12 + '\nSaving results')
 
     data += '\n'
     with open(args.results_file, 'a') as fp:

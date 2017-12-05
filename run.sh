1bddc17a1938a812cee8c857286ba58410dcac61
mcarletti <marco.carletti@univr.it>
2017-12-05 11:01:45 +0100

    Remove run.bat; Simplify run.sh

--- run.sh
+++ run.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
 
-modelname=googlenet
-outdir=results/googlenet_5k
+modelname=alexnet
+outdir=results/alexnet_sample
 outfile=$outdir/results.csv
+
 mkdir -p $outdir
-echo "filename, target_prob, smooth_mask_prob, smooth_drop, smooth_blurred_prob, smooth_p, sharp_mask_prob, sharp_drop, sharp_blurred_prob, sharp_p, spx_mask_prob, spx_drop, spx_blurred_prob, spx_p" > $outfile
+#echo "filename, predicted_class_id, target_prob, smooth_mask_prob, smooth_drop, smooth_blurred_prob, smooth_p, sharp_mask_prob, sharp_drop, sharp_blurred_prob, sharp_p, spx_mask_prob, spx_drop, spx_blurred_prob, spx_p" > $outfile
 
 echo "Save results in $outdir"
 
-for image in "/media/Data/datasets/misc/imagenet_val_5k"/*.JPEG
-#for image in "examples/original"/*.jpg
-do
-    echo "####################################"
-    fname=`echo $image | cut -d '/' -f 7 | cut -d '.' -f 1`
-    #fname=`echo $image | cut -d '/' -f 3 | cut -d '.' -f 1`
-    if [[ "$fname" == *"masked"* ]]; then
-        echo "Skipping $fname"
-    else
-        echo "Processing $image"
-        python3 main.py --modelname $modelname --input_image $image --dest_folder $outdir/$fname --results_file $outfile --super_pixel
-    fi
-done
+#indir=/media/Data/datasets/misc/imagenet_val_5k
+#ext=.JPEG
+
+indir=examples/original/flute3.jpg
+ext=.jpg
+
+echo "####################################"
+
+python3 main.py --modelname $modelname --input_path $indir --dest_folder $outdir --results_file $outfile --file_ext $ext
 
 echo "####################################"
 echo "DONE"

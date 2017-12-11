f3128d266907bfc75eb046ddb40fc2134339a987
mcarletti <marco.carletti@univr.it>
2017-12-12 00:37:01 +0100

    Update evaluation scripts

--- run.sh
+++ run.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-modelname=alexnet
-outdir=results/alexnet_5k
+# --target_id <CLASS_ID> --max_images <MAX>
+
+modelname=googlenet
+#outdir=results/$modelname/$2
+#outdir=results/alexnet_examples_focus
+outdir=results/googlenet_5k
 outfile=$outdir/results.csv
 
 mkdir -p $outdir
-#echo "filename, predicted_class_id, target_prob, smooth_mask_prob, smooth_drop, smooth_blurred_prob, smooth_p, sharp_mask_prob, sharp_drop, sharp_blurred_prob, sharp_p, spx_mask_prob, spx_drop, spx_blurred_prob, spx_p" > $outfile
 
 echo "Save results in $outdir"
 
+#indir=/media/Data/datasets/ImageNet/ILSVRC2012_img_train/$2
+#indir=examples/focus
 indir=/media/Data/datasets/misc/imagenet_val_5k
 ext=.JPEG
 
-#indir=examples/original/
-#ext=.jpg
-
 echo "####################################"
 
-python3 main.py --modelname $modelname --input_path $indir --dest_folder $outdir --results_file $outfile --file_ext $ext $1
+python3 main.py --modelname $modelname --input_path $indir --dest_folder $outdir --results_file $outfile --file_ext $ext "$@"
 
 echo "####################################"
 echo "DONE"
